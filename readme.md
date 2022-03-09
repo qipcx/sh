02f3848a792264727f70d0ab55ac69042cd87235
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-09 22:36:32 +0200

    vps/run-proxy.sh

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Sorbing's Utils
+
+```shell
+https://raw.githubusercontent.com/sorbing/utils/main/vps/run-proxy.sh
+https://cdn.rawgit.com/sorbing/utils/main/vps/run-proxy.sh # Moved Permanently. Redirecting to https://cdn.jsdelivr.net/gh/sorbing/utils@main/vps/run-proxy.sh
+https://cdn.jsdelivr.net/gh/sorbing/utils@main/vps/run-proxy.sh
+```
+
+```shell
+bash <(curl -s https://raw.githubusercontent.com/sorbing/utils/main/vps/run-proxy.sh)
+curl -s https://raw.githubusercontent.com/sorbing/utils/main/vps/run-proxy.sh | bash  ## Нет интерактива
+```
--- vps/run-proxy.sh
+++ vps/run-proxy.sh
@@ -7,13 +7,20 @@ port=$(echo -n "$@" | grep -Po '(?<=--port=)[^ "]+')
 user=$(echo -n "$@" | grep -Po '(?<=--user=)[^ "]+')
 pass=$(echo -n "$@" | grep -Po '(?<=--pass=)[^ "]+')
 
-test -z "$port" && read -r -p "Proxy port:" port
+rand_pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9@#_%^&' | fold -w 12 | head -n 1)
+
+test -z "$port" && read -r -p "Proxy port: " port
 test -z "$user" && read -r -p "Proxy user: " user
-test -z "$pass" && read -r -p "Proxy pass: " pass
+test -z "$pass" && read -r -p "Proxy pass ($rand_pass): " pass
+pass=${pass:-$rand_pass}
 
 command -v pip &> /dev/null || sudo apt install pip -y
 command -v proxy &> /dev/null || sudo pip install --upgrade --prefix /usr/local proxy.py
+
 #sudo iptables -I INPUT 5 -i ens3 -p tcp --dport "$port" -m state --state NEW,ESTABLISHED -j ACCEPT
+
+cmd="proxy --hostname 0.0.0.0 --port $port --basic-auth \"$user:$pass\""
 echo "See: https://github.com/abhinavsingh/proxy.py#start-proxypy"
-echo "IP: $(curl ifconfig.co)"
-proxy --hostname 0.0.0.0 --port "$port" --basic-auth "$user:$pass"
+echo "IP: $(curl -s checkip.amazonaws.com)"
+echo "$cmd"
+eval "$cmd"
