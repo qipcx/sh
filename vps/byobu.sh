f392d2f5bf38ff8bc2aa22961f52137e55d03013
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-10 15:27:44 +0200

    vps/byobu.sh

--- vps/byobu.sh
+++ vps/byobu.sh
@@ -3,14 +3,14 @@
 if command -v apt &> /dev/null; then command -v byobu &> /dev/null || sudo apt install byobu -y; fi
 
 (nohup byobu >/dev/null 2>&1 &) > /dev/null
-sleep 2
+sleep 3
 kill $(ps aux | grep byobu-shell | awk '{print $2}') >/dev/null 2>&1
 
 status_path=~/.byobu/status
 
-if [[ -f status_path ]]; then
+if [[ -f $status_path ]]; then
   sed -i 's/^tmux_left=.*$/tmux_left="logo #distro #release #arch #whoami #hostname session"/' $status_path
-  sed -i 's/^tmux_right=.*$/tmux_right="network raid services processes load_average cpu_count cpu_freq memory swap disk disk_io uptime distro release updates_available reboot_required apport ip_address date time"/' status_path
+  sed -i 's/^tmux_right=.*$/tmux_right="network raid services processes load_average cpu_count cpu_freq memory swap disk disk_io uptime distro release updates_available reboot_required apport ip_address date time"/' $status_path
 
   byobu-enable
   echo "Run: byobu"
