73407bd02b8418e2858ae2779e850bc29f7c3b38
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-16 15:44:59 +0200

    vps/byobu.sh, vps/terminal.sh

--- vps/byobu.sh
+++ vps/byobu.sh
@@ -1,26 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if command -v apt &> /dev/null; then command -v byobu &> /dev/null || sudo apt install byobu -y; fi
 if command -v yum &> /dev/null; then command -v byobu &> /dev/null || sudo yum install byobu -y; fi ## sudo yum check-update; yum info byobu2 && echo OK || echo NO
 
-(nohup byobu >/dev/null 2>&1 &) > /dev/null
-sleep 3
-kill $(ps aux | grep byobu-shell | awk '{print $2}') >/dev/null 2>&1
-
 status_path=~/.byobu/status
 
-if [[ -f $status_path ]]; then
+## @note Hack. Init the ~/.byobu directory & configs
+if [ ! -f $status_path ]; then
+  (nohup byobu >/dev/null 2>&1 &) > /dev/null
+  sleep 3
+  kill $(ps aux | grep byobu-shell | awk '{print $2}') >/dev/null 2>&1
+fi
+
+if [ ! -f $status_path ]; then
   sed -i 's/^tmux_left=.*$/tmux_left="logo #distro #release #arch #whoami #hostname session"/' $status_path
   sed -i 's/^tmux_right=.*$/tmux_right="network raid services processes load_average cpu_count cpu_freq memory swap disk disk_io uptime distro release updates_available reboot_required apport ip_address custom date time"/' $status_path
 
   ## Test Real IP
-  if [[ -d  /usr/lib/byobu/ ]]; then
+  if [ -d  /usr/lib/byobu/ ]; then
     . /usr/lib/byobu/include/dirs
     . /usr/lib/byobu/include/shutil
     . /usr/lib/byobu/ip_address
   fi
 
-  if [[ -d /usr/libexec/byobu/ ]]; then # centos 7
+  if [ -d /usr/libexec/byobu/ ]; then # centos 7
     . /usr/libexec/byobu/include/dirs
     . /usr/libexec/byobu/include/shutil
     . /usr/libexec/byobu/ip_address
@@ -29,18 +32,24 @@ if [[ -f $status_path ]]; then
   byobu_ip=$(__ip_address t 2> /dev/null)
   real_ip=$(curl -s http://checkip.amazonaws.com/)
 
-  if [[ "$byobu_ip" != "$real_ip" ]]; then
+  if [ "$byobu_ip" != "$real_ip" ]; then
     echo "You byobu IP $byobu_ip is not equal real IP $real_ip"
-    read -r -n 1 -p "Switch ~/.byobu/statusrc config to show real IP? [Y/n] " reply
-    if [[ -z "$reply" || "$reply" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    #read -r -n 1 -p "Switch ~/.byobu/statusrc config to show real IP? [Y/n] " reply
+    #if [[ -z "$reply" || "$reply" =~ ^([yY][eE][sS]|[yY])$ ]]; then  ## @todo Replace for sh: 35: Syntax error: "(" unexpected (expecting "then")
+    if true; then
       echo
       sed -i 's/.*IP_EXTERNAL=.*$/IP_EXTERNAL=1/' ~/.byobu/statusrc
       echo "Changed: IP_EXTERNAL=1 in the ~/.byobu/statusrc"
+      echo "You can change it:"
+      echo "sed -i 's^IP_EXTERNAL=1^IP_EXTERNAL=0^' ~/.byobu/statusrc"
+      echo "sed -i 's^IP_EXTERNAL=0^IP_EXTERNAL=1^' ~/.byobu/statusrc"
+      echo
     fi
   fi
 
   #byobu-enable
   echo "Run: byobu"
+  byobu
 else
   echo "⚠ Config not found: $status_path"
   echo "Try start byobu and run script again"
--- vps/terminal.sh
+++ vps/terminal.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+bind -p > ~/.inputrc-restore
+
+bind '"\C-h": backward-kill-word' # Ctrl+Backspace
+bind '"\e[3;5~": kill-word'       # Ctrl+Delete
+bind '"\e[1;5D": backward-word'   # Ctrl+Left
+bind '"\e[1;5C": forward-word'    # Ctrl+Right
+bind '"\e[A": history-search-backward' # History search by begin text
+bind '"\e[B": history-search-forward'  # History search by begin text
+
+# stty werase ^H; stty kill ^K; bind '"\C-u": undo';
+
 tee ~/.inputrc cat > /dev/null <<EOT
 # Ctrl+Backspace
 "\C-h": backward-kill-word
@@ -11,10 +22,13 @@ tee ~/.inputrc cat > /dev/null <<EOT
 "\e[1;5D": backward-word
 "\e[1;5C": forward-word
 
-# История команд по начальному вхождению
+# History search by begin text
 "\e[A": history-search-backward
 "\e[B": history-search-forward
 
 # Дополнение Tab, двойной Tab
 #set show-all-if-ambiguous on
 EOT
+
+echo "Use to restore/reset settings:"
+echo 'stty sane && bind -f ~/.inputrc-restore'
