2612dfd577e19b897fd2f7d3d75df34ab888738f
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-17 04:55:12 +0200

    vps/terminal.sh

--- vps/terminal.sh
+++ vps/terminal.sh
@@ -9,32 +9,28 @@ fi
 
 bind -p > ~/.inputrc-restore
 
-bind '"\C-h": backward-kill-word' # Ctrl+Backspace
-bind '"\e[A": history-search-backward' # History search by begin text
-bind '"\e[B": history-search-forward'  # History search by begin text
-
-stty kill undef
-bind '"\C-u": undo'
-stty kill ^K
+#bind '"\C-h": backward-kill-word' # Ctrl+Backspace
+#bind '"\C-k": unix-line-discard'  # Kill line
+#bind '"\C-j": undo'  # Undo kill
+#bind '"\e[A": history-search-backward' # History search by begin text
+#bind '"\e[B": history-search-forward'  # History search by begin text
 
 tee ~/.inputrc cat > /dev/null <<EOT
 # Ctrl+Backspace
 "\C-h": backward-kill-word
 
-# Ctrl+Delete
-#"\e[3;5~": kill-word
-
-# Ctrl+Left/Right
-#"\e[1;5D": backward-word
-#"\e[1;5C": forward-word
+"\C-k": unix-line-discard
+"\C-j": undo
 
 # History search by begin text
 "\e[A": history-search-backward
 "\e[B": history-search-forward
 
-# Дополнение Tab, двойной Tab
+# Дополнение по одиночному Tab, вместо двойной Tab
 #set show-all-if-ambiguous on
 EOT
+
+bind -f ~/.inputrc
 
 echo "Use to restore/reset settings:"
 echo 'stty sane && bind -f ~/.inputrc-restore'
