9cb56eb3b695ff180b52b8da4f1a1d74b7382e09
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-19 00:14:36 +0200

    vps/proxy.sh

--- vps/proxy.sh
+++ vps/proxy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-port=$(echo -n "$@" | grep -Po '(?<=--port=)[^ "]+')
-user=$(echo -n "$@" | grep -Po '(?<=--user=)[^ "]+')
-pass=$(echo -n "$@" | grep -Po '(?<=--pass=)[^ "]+')
+[ "$(readlink /proc/$$/exe)" = /usr/bin/bash ] || { curl -sL qip.cx/vps/proxy.sh | bash -s -- "$@"; exit; } ## Run in bash
 
-# @todo Alternative format: --port 56293 --basic-auth user:pass
+function randval() {
+  cat /dev/urandom | tr -dc "${1:-"a-zA-Z0-9"}" | fold -w "${2:-12}" | head -n 1
+}
 
-rand_user=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 6 | head -n 1)
-rand_pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9#_%^&' | fold -w 12 | head -n 1)
-rand_port="5"$(cat /dev/urandom | tr -dc '0-9' | fold -w 4 | head -n 1)
+port=$(echo -n "$@" | grep -Po '(?<=--port[=| ])[^ "]+')
+user=$(echo -n "$@" | grep -Po '(?<=--user[=| ])[^ "]+')
+pass=$(echo -n "$@" | grep -Po '(?<=--pass[=| ])[^ "]+')
 
-test -z "$user" && read -r -p "Proxy user ($rand_user): " user
-test -z "$pass" && read -r -p "Proxy pass ($rand_pass): " pass
-test -z "$port" && read -r -p "Proxy port ($rand_port): " port
+rand_user=$(randval 'a-z' 6)
+rand_pass=$(randval 'a-zA-Z0-9#_%^&' 12)
+rand_port="5"$(randval '0-9' 4)
+
+test -n "$user" || read -r -p "Proxy user ($rand_user): " user </dev/tty
+test -z "$pass" && read -r -p "Proxy pass ($rand_pass): " pass </dev/tty
+test -z "$port" && read -r -p "Proxy port ($rand_port): " port </dev/tty
 
 user=${user:-$rand_user}
 pass=${pass:-$rand_pass}
@@ -25,11 +29,15 @@ test -z "$pass" && { echo "Not specified --pass!"; exit 3; }
 ## Ubuntu/Debian
 if command -v apt &> /dev/null; then command -v pip &> /dev/null || sudo apt install python3-pip -y; fi
 command -v proxy &> /dev/null || sudo pip install --upgrade --prefix /usr/local proxy.py
-sudo iptables -I INPUT 5 -p tcp --dport "$port" -m state --state NEW,ESTABLISHED -j ACCEPT
+
+command -v proxy &> /dev/null || { echo "Не удалось установить proxy.py!"; exit 9; }
+
+sudo iptables -I INPUT 1 -p tcp --dport "$port" -m state --state NEW,ESTABLISHED -j ACCEPT
+# OR: sudo iptables -I INPUT 1 -i ens3 -p tcp --dport XXXXX -j ACCEPT
+# OR: sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
+cmd="proxy --hostname 0.0.0.0 --port $port --basic-auth $user:$pass"
 
 ip=$(curl -s 2ip.fun)
-
-cmd="proxy --hostname 0.0.0.0 --port $port --basic-auth $user:$pass"
 
 echo
 echo "See: https://github.com/abhinavsingh/proxy.py#start-proxypy"
